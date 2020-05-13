df18b469a9521a2493fca69f592d8116ad5d9af7
Ann <panfilovaania@mail.ru>
2020-05-13 23:28:15 +0300

    tvoi comment

--- kineckt/Assets/Scripts/TutorSystem/GeneralSet.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class GeneralSet
-{
-    public static bool tutor = false;
-    public static int _GameId = 1;
-}
